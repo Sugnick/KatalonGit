4e89666d45056bfa80f8a491c370c24bfadd5d91
sugnick004 <89131440+sugnick004@users.noreply.github.com>
2021-11-24 17:56:36 +0530

    test

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2f8975af-3fe1-461f-b45d-a42c8917d576</testSuiteGuid>
+   <testCaseLink>
+      <guid>34cd87b5-0540-4df1-9a52-571c546a8ab7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NotepadCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
